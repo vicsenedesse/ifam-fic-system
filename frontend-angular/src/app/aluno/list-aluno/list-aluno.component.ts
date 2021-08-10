0d7a0c5668ed94c0955d301bedcb25840baca21c
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2021-08-10 17:31:50 -0400

    ' '

--- frontend-angular/src/app/aluno/list-aluno/list-aluno.component.ts
+++ frontend-angular/src/app/aluno/list-aluno/list-aluno.component.ts
@@ -8,15 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListAlunoComponent implements OnInit {
 
-  constructor(
-    private elemento: ElementRef
-  ) { }
+  constructor() { }
 
   ngOnInit() {
   }
-
-  ngAfterViewInit(): void {
-      this.elemento.nativeElement.ownerDocument.body.style.background = 'url(../../../../../assets/WhatsApp Image 2021-08-10 at 17.00.24.jpeg)';
-  }
-
 }
