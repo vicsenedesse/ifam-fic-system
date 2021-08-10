0e088756e41d9c84764ca719c6df4965b3cf0f52
raphaela araujo <rph.goes@gmail.com>
2021-08-10 16:49:02 -0300

    Criando as pastas do CRUD turma

---
+++ frontend-angular/src/app/turma/list-turma/list-turma.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-turma',
+  templateUrl: './list-turma.component.html',
+  styleUrls: ['./list-turma.component.css']
+})
+export class ListTurmaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
