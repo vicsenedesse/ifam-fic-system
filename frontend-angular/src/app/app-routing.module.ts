9e95c30ca33c7b676ad926aaeaa71b314aefd401
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2021-08-10 15:52:57 -0400

    Configurações inicias das rotas #1

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
+import { CursoFicComponent } from './curso-fic/curso-fic.component';
+import { TurmaComponent } from './turma/turma.component';
+import { ProfessorComponent } from './professor/professor.component';
+import { AlunoComponent } from './aluno/aluno.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 
+  {path:'cursos', component:CursoFicComponent},
+  {path:'turmas', component:TurmaComponent},
+  {path:'professor', component:ProfessorComponent},
+  {path:'aluno', component:AlunoComponent},
   {path:'home', component:HomeComponent},
   {path:'', component:HomeComponent},
 
