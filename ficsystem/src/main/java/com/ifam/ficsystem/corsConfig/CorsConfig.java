188cc6eb9a0a4a602423b6ad322cccbf61fa71ca
vicsenedesse <engsoftvicsenedesse@gmail.com>
2021-08-19 12:05:45 -0400

    Backend esta linkando com o banco Mysql #1

--- ficsystem/src/main/java/com/ifam/ficsystem/corsConfig/CorsConfig.java
+++ ficsystem/src/main/java/com/ifam/ficsystem/corsConfig/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${allowed.origin}")
+    // @Value("${allowed.origin}")
     private String allowedOrigin;
 
     @Bean
